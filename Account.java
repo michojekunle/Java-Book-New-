5eadaa5bc09e956413e2f9b6925c105d23686758
michojekunle <90222287+michojekunle@users.noreply.github.com>
2021-10-14 13:00:14 +0100

    good

--- Account.java
+++ Account.java
@@ -5,18 +5,43 @@ package JavaBookPack;
 public class Account {
 
 	private String name; // instance variable
+	private double balance; // instance variable
+	
+	// Account constructor that receives two parameters 
+	public Account(String name, double balance) 
+	{
+		this.name = name; // assign name to instance variable name 
+		
+		//validate that the balance is greater than 0.0
+		if(balance > 0.0) //if the balance is valid
+			this.balance = balance ; //assign it to the instance variable balance
+	}
+	
+	// method that deposits (adds) only a valid amount to the balance 
+	public void deposit(double depositAmount) 
+	{
+		if(depositAmount > 0.0) //if the deposit Amount is valid 
+			balance = balance + depositAmount; // Adds it to the balance
+	}
+	
+	//method that returns the Account balance
+	public double getBalance() 
+	{
+		return balance;
+	} 
+	
+	// method to set the name in the object 
+	public void setName(String name) 
+	{
+		this.name = name; // store the name 
+	}
 			
-			// method to set the name in the object 
-			public void setName(String name) 
-			{
-				this.name = name; // store the name 
-			}
-				
-			// Method to retrieve the name from the object 
-			public String getName()
-			{
-				return name; // Return value of name to the caller 
-			}
+	// Method to retrieve the name from the object 
+	public String getName()
+	{
+		return name; // Return value of name to the caller 
+	
+	}///end method getName
 }//end class Account
 
 
--- AccountTest.java
+++ AccountTest.java
@@ -1,10 +1,45 @@
 package JavaBookPack;
-
-public class AccountTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//Fig 3.2: AccountTest.java
+//Creating and Manipulating an Account Object. 
+import java.util.Scanner;
+public class AccountTest 
+{
+	public static void main(String[] args) 
+	{
+		
+		Account account1 = new Account("Jana Green", 50.00);
+		Account account2 = new Account("John Blue", -7.53); 
+		
+		//display initial balance of each object 
+		System.out.printf("%s balance: %.2f%n", account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: %.2f%n", account2.getName(), account2.getBalance());
+		
+		//create a Scanner to obtain input from the command window 
+		Scanner input = new Scanner(System.in);
+		
+		System.out.printf("Enter deosit Amount for account1: "); //prompt 
+		double depositAmount = input.nextDouble(); //obtain user input 
+		System.out.printf("%nadding  %.2f to account1 balance %n%n", 
+				depositAmount);
+		account1.deposit(depositAmount); //add to account1's balance
+		
+		//display balances 
+		System.out.printf("%s balance: $%.2f%n",
+				account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: $%.2f%n",
+				account2.getName(), account2.getBalance());
+		
+		System.out.printf("Enter deosit Amount for account2: "); //prompt 
+		depositAmount = input.nextDouble(); //obtain user input 
+		System.out.printf("%nadding  %.2f to account2 balance %n%n", 
+				depositAmount);
+		account2.deposit(depositAmount); //add to account2's balance
+		
+		//display balances 
+		System.out.printf("%s balance: $%.2f%n",
+				account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: $%.2f%n",
+				account2.getName(), account2.getBalance());
+		
 	}
-
-}
+}//end class AccountTest
